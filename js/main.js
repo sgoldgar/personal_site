9eb9d6c1c4e653a3580b81c549b35f3bb5b4b8ac
Sarah Goldgar <sgoldgar@gmail.com>
2017-04-03 14:15:08 -0500

    changed layout

---
+++ js/main.js
@@ -0,0 +1,117 @@
+$(document).ready(function(){
+
+/*Carousel*/
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
+
+/*Add Hello on Logo Hover*/
+$('.my-logo').hover(
+  function() {
+    $( this ).append( $('<div class="hello"> Hello!</div>') );
+  }, function() {
+    $( this ).find(".hello").remove();
+  }
+);
+
+
+/*Hamburger Toggle for Mobile*/
+var menuStatus = ($('#menu-icon').css('display'));
+if(menuStatus == "block" || menuStatus == "inline-block") {
+  $('nav').hide();
+  $('#menu-icon').on('click', function(){
+    $('nav').toggle();
+    console.log('click');
+  });
+}
+
+/*Scroll to Sections*/
+
+function scroll(target){
+  $('html, body').animate({
+    scrollTop: $(target).offset().top - 150}, 'slow');
+}
+
+
+  /*Scroll to top of page when click logo*/
+  $('.my-logo').on('click', function(){
+    $('html,body').scrollTop(0);
+    console.log('top');
+  });
+
+  /*Scroll to about when click on nav link*/
+$('nav ul li:first-child').first().on('click', function(){
+  $(this).addClass('selected');
+  $(this).siblings().removeClass('selected');
+  var target = '.about';
+  scroll(target);
+  if(menuStatus == "block" || menuStatus == "inline-block") {
+    $('nav').hide();
+  }
+  console.log('about');
+});
+  /*Scroll to about when click on carousel link*/
+$('.carousel-inner .item h3 a').on('click', function(){
+  $('nav ul li:first-child').first().addClass('selected');
+  $('nav ul li:first-child').first().siblings().removeClass('selected');
+  var target = '.about';
+  scroll(target);
+  console.log('about');
+});
+  /*Scroll to projects when click on carousel link*/
+$('.carousel-inner .item h3 a').eq(2).on('click', function(){
+  $('nav ul li').eq(2).first().addClass('selected');
+  $('nav ul li').eq(2).first().siblings().removeClass('selected');
+  var target = '.projects';
+  scroll(target);
+  console.log('projects');
+});
+
+
+    /*Scroll to projects when click on nav link*/
+$('nav ul li').eq(2).first().on('click', function(){
+  $(this).addClass('selected');
+  $(this).siblings().removeClass('selected');
+  var target = '.projects';
+  scroll(target);
+  if(menuStatus == "block" || menuStatus == "inline-block") {
+    $('nav').hide();
+  }
+  console.log('projects');
+});
+
+    /*Scroll to contact when click on nav link*/
+$('nav ul li').eq(3).first().on('click', function(){
+  $(this).addClass('selected');
+  $(this).siblings().removeClass('selected');
+  var target = '.contact';
+  scroll(target);
+  if(menuStatus == "block" || menuStatus == "inline-block") {
+    $('nav').hide();
+  }
+  console.log('contact');
+});
+
+
+
+/*Lightbox Functionality*/
+$(".about .circle").each(function(index, element) {
+  $(this).on('click', function(){
+    $(this).next().fadeIn(300) && $('.lightbox-background').children().fadeIn(300);
+  });
+  $(".close, .lightbox-background").on('click', function(){
+    $(".lightbox, .lightbox-background").fadeOut(300);
+  });
+});
